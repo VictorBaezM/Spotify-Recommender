f056d2610264f2e71fb72033edcf95514e2f6433
VictorBaezM <vmorales34@gatech.edu>
2026-05-31 15:02:34 -0400

    chore: update build base paths and redirect URIs to Spotify-Recommender

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/spotify-music-recommender/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/Spotify-Recommender/' : '/',
   plugins: [
     react(),
     tailwindcss(),
